b6796c6534b3cc37f370c51e0f9d228440febe39
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2026-03-10 15:20:27 +0100

    Add people task placeholder

---
+++ src/AgentFive/Agents/OpenRouterAgent.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AgentFive.Agents;
+
+public class OpenRouterAgent
+{
+
+}
---
+++ src/AgentFive/Tasks/People/Person.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AgentFive.Tasks.People;
+
+public class Person
+{
+
+}
